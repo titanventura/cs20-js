845b211bffe054ea4a7ec15debdb8815476186d9
AbhayMohanan <abhay.thamarassery@gmail.com>
2022-04-17 17:58:34 +0530

    Added cpu element

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
+//reference to each element
 let player_rock = document.getElementById("rock");
 let player_paper = document.getElementById("paper")
 let player_scissor = document.getElementById("scissor")
 
+ //Player => Rock
 player_rock.addEventListener("click",function(){
-	console.log("rock");
+	
+	cpu_element = get_cpu_element();
+	console.log(cpu_element);
 })
 
+//Player => Paper
 player_paper.addEventListener("click",function(){
-	console.log("paper");
+	
+	cpu_element = get_cpu_element();
+	console.log(cpu_element);
 })
 
+//Player => Scissor
 player_scissor.addEventListener("click",function(){
-	console.log("scissor");
+	
+	cpu_element = get_cpu_element();
+	console.log(cpu_element);
 })
+
+function get_cpu_element() {
+	// 1 ==> rock; 2 ==> paper; 3 ==> scissor 
+	let cpu_element = Math.floor((Math.random() * 3) + 1);
+
+	return cpu_element;
+}
