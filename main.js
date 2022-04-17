3bc19269a67ea7dde379a0c71f15807d3312764a
AbhayMohanan <abhay.thamarassery@gmail.com>
2022-04-17 17:47:04 +0530

    Added event listener for each element

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-	Array.from(document.getElementsByClassName("element")).forEach((el) => {
-		el.addEventListener("click", (event) => {
-			console.log(event.target);
-		});
-	});
-});
+let player_rock = document.getElementById("rock");
+let player_paper = document.getElementById("paper")
+let player_scissor = document.getElementById("scissor")
+
+player_rock.addEventListener("click",function(){
+	console.log("rock");
+})
+
+player_paper.addEventListener("click",function(){
+	console.log("paper");
+})
+
+player_scissor.addEventListener("click",function(){
+	console.log("scissor");
+})
