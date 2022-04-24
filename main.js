20d7d121256bcb524076f7b2fc366b7e89778e02
Santhosh-S-J <santhosh_sj@outlook.com>
2022-04-24 23:12:47 +0530

    Added Js to rock paper scissors game

--- main.js
+++ main.js
@@ -5,3 +5,74 @@ document.addEventListener("DOMContentLoaded", () => {
 		});
 	});
 });
+
+const choices = ["Rock", "Paper", "Scissors"];
+
+const rock =  document.getElementById("rock");
+const paper =  document.getElementById("paper");
+const scissors =  document.getElementById("scissor");
+
+var user_score = 0;
+var comp_score = 0;
+
+rock.addEventListener("click", function(){
+	user_choice = "Rock";
+	console.log(user_choice);
+	var comp_choice = choices[Math.floor(Math.random() * choices.length)];
+	console.log(comp_choice);
+
+	if(comp_choice == "Paper"){
+		document.getElementById("info").innerHTML = '<span style="font-weight: bold; font-size: 30px"> Computer Wins </font>';
+		comp_score++; 
+		document.getElementById("computer_score").innerHTML = comp_score;
+	}
+	else if(comp_choice == "Scissors"){
+		document.getElementById("info").innerHTML = '<span style="font-weight: bold; font-size: 30px"> You Win </font>';
+		user_score++; 
+		document.getElementById("human_score").innerHTML = user_score;
+	}
+	else{
+		document.getElementById("info").innerHTML = '<span style="font-weight: bold; font-size: 30px"> Draw </font>';
+	}
+});
+
+paper.addEventListener("click", function(){
+	user_choice = "Paper";
+	console.log(user_choice);
+	comp_choice = choices[Math.floor(Math.random() * choices.length)];
+	console.log(comp_choice);
+	if(comp_choice == "Scissors"){
+		document.getElementById("info").innerHTML = '<span style="font-weight: bold; font-size: 30px"> Computer Wins </font>';
+		comp_score++; 
+		document.getElementById("computer_score").innerHTML = comp_score;
+	}
+	else if(comp_choice == "Rock"){
+		document.getElementById("info").innerHTML = '<span style="font-weight: bold; font-size: 30px"> You Win </font>';
+		user_score++;
+		document.getElementById("human_score").innerHTML = user_score;
+	}
+	else{
+		document.getElementById("info").innerHTML = '<span style="font-weight: bold; font-size: 30px"> Draw </font>';
+	}
+});
+
+scissors.addEventListener("click", function(){
+	user_choice = "Scissors";
+	console.log(user_choice);
+	comp_choice = choices[Math.floor(Math.random() * choices.length)];
+	console.log(comp_choice);
+	if(comp_choice == "Rock"){
+		document.getElementById("info").innerHTML = '<span style="font-weight: bold; font-size: 30px"> Computer Wins </font>';
+		comp_score++; 
+		document.getElementById("computer_score").innerHTML = comp_score;
+	}
+	else if(comp_choice == "Paper"){
+		document.getElementById("info").innerHTML = '<span style="font-weight: bold; font-size: 30px"> You Win </font>';
+		user_score++;
+		document.getElementById("human_score").innerHTML = user_score;
+	}
+	else{
+		document.getElementById("info").innerHTML = '<span style="font-weight: bold; font-size: 30px"> Draw </font>';
+	}
+});
+
